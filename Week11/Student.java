ae9032a5ffef1e0cb559508f0ef4097b47a2a752
Panadon-s <77398559+Panadon-s@users.noreply.github.com>
2021-03-08 23:01:55 +0700

    Add files via upload

---
+++ Week11/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    String name = "name";
+    int age = 0;
+    double gpa = 0.00;
+
+public Student (String name,int age,double gpa) {
+    this.name = name;
+    this.age = age;
+    this.gpa =gpa;
+    }
+public boolean equals(Student std00) {
+        if(std00 instanceof Student) {
+            return name == ((Student)std00).name;
+        }else{
+            return false;
+            }
+        }
+public boolean ageEquals(Student std00) {
+        if(std00 instanceof Student) {
+            return age == ((Student)std00).age;
+        }else{
+            return false;
+            }
+        }
+public boolean gpaEquals(Student std00) {
+        if(std00 instanceof Student) {
+            return gpa == ((Student)std00).gpa;
+        }else{
+            return false;
+            }
+        }
+    }
